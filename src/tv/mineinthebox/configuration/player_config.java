2c73798dda0dac6a57fd6068a102e2c1d5a45b50
xize <glucassen@live.nl>
2013-08-15 18:58:58 +0200

    added player configuration

--- src/tv/mineinthebox/configuration/player_config.java
+++ src/tv/mineinthebox/configuration/player_config.java
@@ -1,9 +1,16 @@
 package tv.mineinthebox.configuration;
 
+import tv.mineinthebox.fileManager;
+
 public class player_config {
 	
-	public void test() {
-		
+	public void create_player() {
+		if(!fileManager.file_exists("player.yml", fileManager.getDir())) {
+			fileManager.createHeader("player.yml", "default configuration for players", fileManager.getDir());
+			fileManager.writeFile("player.yml", "save-playerInventory", true, fileManager.getDir());
+			fileManager.writeFile("player.yml", "godmode-inAfk", true, fileManager.getDir());
+			fileManager.writeFile("player.yml", "steve-hurt-sound.enable", false, fileManager.getDir());
+		}
 	}
 
 }
