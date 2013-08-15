b07a8ce2ad71491535eef09e1206dc3e8c3b462d
xize <glucassen@live.nl>
2013-08-15 20:20:28 +0200

    added enums for events

---
+++ src/tv/mineinthebox/events/enumTypes/playerJoinEnum.java
@@ -0,0 +1,5 @@
+package tv.mineinthebox.events.enumTypes;
+
+public enum playerJoinEnum {
+	altmessage, welcomeMessage, isXeph0re, isWild, isSafe
+}
---
+++ src/tv/mineinthebox/events/enumTypes/playerMoveEventEnum.java
@@ -0,0 +1,5 @@
+package tv.mineinthebox.events.enumTypes;
+
+public enum playerMoveEventEnum {
+	isWild, isSafe
+}
