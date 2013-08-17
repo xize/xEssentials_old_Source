bab84a5bd99928a6c7cdde1dd2ab415192b8b8d3
xize <glucassen@live.nl>
2013-08-17 16:23:49 +0200

    updated playerPermission

--- src/tv/mineinthebox/permissions/playerPermission.java
+++ src/tv/mineinthebox/permissions/playerPermission.java
@@ -1,5 +1,15 @@
 package tv.mineinthebox.permissions;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
 public class playerPermission {
+	
+	public static void getPermissionError(CommandSender sender, Command cmd, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("sethome")) {
+			sender.sendMessage(ChatColor.RED + "you are not allowed to use this command! /" + cmd.getName() + "\n" + ChatColor.GRAY + "permission: xEssentials.command.sethome");
+		}
+	}
 
 }
