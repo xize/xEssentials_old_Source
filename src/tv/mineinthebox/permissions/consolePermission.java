b02274e83ca8f3f1991d6e25053b40d9c17e5f17
xize <glucassen@live.nl>
2013-08-17 16:20:18 +0200

    update console warning

--- src/tv/mineinthebox/permissions/consolePermission.java
+++ src/tv/mineinthebox/permissions/consolePermission.java
@@ -1,5 +1,12 @@
 package tv.mineinthebox.permissions;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
 public class consolePermission {
+	
+	public static void getConsoleMessage(CommandSender sender) {
+		sender.sendMessage(ChatColor.RED + "cannot perform any command on non living entities!");
+	}
 
 }
