8441a5986b85d1707773de57a0a6a1d44e5a500a
xize <glucassen@live.nl>
2013-08-18 14:57:56 +0200

    fixed kick messages to be ignored for banned users and added console reasons

--- src/tv/mineinthebox/events/playerKickEvent.java
+++ src/tv/mineinthebox/events/playerKickEvent.java
@@ -1,8 +1,14 @@
 package tv.mineinthebox.events;
 
+import java.io.File;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
+
+import tv.mineinthebox.fileManager;
+import tv.mineinthebox.resources.bansystem.ban;
+import tv.mineinthebox.resources.timeunit.timeunits;
 
 public class playerKickEvent implements Listener {
 	
@@ -10,6 +16,19 @@ public class playerKickEvent implements Listener {
 	public void fireFly(PlayerKickEvent e) {
 		if(playermove.firefly.contains(e.getPlayer().getName())) {
 			playermove.firefly.remove(e.getPlayer().getName());
+		}
+	}
+	
+	@EventHandler
+	public void bannedPlayer(PlayerKickEvent e) {
+		if(ban.isBanned(e.getPlayer())) {
+			e.setLeaveMessage("");
+			e.setReason(e.getPlayer().getName() + " is banned");
+		} else if(ban.isTempBanned(e.getPlayer())) {
+			if(!timeunits.isOverTime(fileManager.getLongValue(e.getPlayer().getName() + ".yml", "time", fileManager.getDir() + File.separator + "bans"))) {
+				e.setLeaveMessage("");
+				e.setReason(e.getPlayer().getName() + " is tempory banned till " + timeunits.getElapsedTime(fileManager.getLongValue(e.getPlayer().getName() + ".yml", "time", fileManager.getDir() + File.separator + "bans")));	
+			}
 		}
 	}
 
