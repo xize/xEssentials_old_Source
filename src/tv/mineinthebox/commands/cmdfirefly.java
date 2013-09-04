67c747e27b8de3274783c97b67d2250ffbd34434
xize <glucassen@live.nl>
2013-09-04 21:35:33 +0200

    added fixes for firefly for the new event system

--- src/tv/mineinthebox/commands/cmdfirefly.java
+++ src/tv/mineinthebox/commands/cmdfirefly.java
@@ -5,31 +5,35 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import tv.mineinthebox.events.playermove;
+import tv.mineinthebox.xEssentialsMemory;
 import tv.mineinthebox.permissions.consolePermission;
 import tv.mineinthebox.permissions.playerPermission;
 
 public class cmdfirefly {
-	
+
 	public static boolean execute(CommandSender sender, Command cmd, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("firefly")) {
 			if(sender instanceof Player) {
+				Player p = (Player) sender;
 				if(sender.hasPermission("xEssentials.command.firefly")) {
-					if(playermove.firefly.contains(sender.getName())) {
-						playermove.firefly.remove(sender.getName());
-						sender.sendMessage(ChatColor.GREEN + "disabled firefly!");
+					if(xEssentialsMemory.returnPlayer(p).containsKey("firefly")) {
+						if((Boolean) xEssentialsMemory.returnPlayer(p).get("firefly")) {
+							xEssentialsMemory.returnPlayer(p).put("firefly", false);
+							xEssentialsMemory.updatePlayerConfig(p);
+							sender.sendMessage(ChatColor.GREEN + "disabled firefly!");
+						} else {
+							xEssentialsMemory.returnPlayer(p).put("firefly", true);
+							xEssentialsMemory.updatePlayerConfig(p);
+							sender.sendMessage(ChatColor.GREEN + "enabled firefly!");
+						}
 					} else {
-						playermove.firefly.add(sender.getName());
-						sender.sendMessage(ChatColor.GREEN + "enabled firefly!");
+						playerPermission.getPermissionError(sender, cmd, args);
 					}
 				} else {
-					playerPermission.getPermissionError(sender, cmd, args);
+					consolePermission.getConsoleMessage(sender);
 				}
-			} else {
-				consolePermission.getConsoleMessage(sender);
 			}
 		}
 		return false;
 	}
-
 }
