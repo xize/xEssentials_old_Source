4abe7173aff727ef42974696b973ce23dfb7e91d
xize <glucassen@live.nl>
2013-09-08 18:48:00 +0200

    fixed wrong event call for alt check

--- src/tv/mineinthebox/events/leaveEvent/saveLeaveAlt.java
+++ src/tv/mineinthebox/events/leaveEvent/saveLeaveAlt.java
@@ -4,14 +4,14 @@ import java.io.File;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import tv.mineinthebox.fileManager;
 
 public class saveLeaveAlt implements Listener {
 	
 	@EventHandler
-	public void saveAlt(PlayerKickEvent e) {
+	public void saveAlt(PlayerQuitEvent e) {
 		String ip = e.getPlayer().getAddress().getHostName();
 		fileManager.writeFile(e.getPlayer().getName() + ".yml", "ip", ip, fileManager.getDir() + File.separator + "alts");
 		fileManager.writeFile(e.getPlayer().getName() + ".yml", "player", e.getPlayer().getName(), fileManager.getDir() + File.separator + "alts");
