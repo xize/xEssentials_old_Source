e7973f595402814f3800e43a2c3d683a97f485f2
xize <glucassen@live.nl>
2013-09-16 20:14:52 +0200

    added fixes for memory usage

--- src/tv/mineinthebox/events/kickEvent/delKickMemory.java
+++ src/tv/mineinthebox/events/kickEvent/delKickMemory.java
@@ -1,13 +1,14 @@
 package tv.mineinthebox.events.kickEvent;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 
 import tv.mineinthebox.xEssentialsMemory;
 
 public class delKickMemory implements Listener {
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void delPlayerMemory(PlayerKickEvent e) {
 		xEssentialsMemory.updatePlayerConfig(e.getPlayer());
 		xEssentialsMemory.returnPlayer(e.getPlayer()).clear();
