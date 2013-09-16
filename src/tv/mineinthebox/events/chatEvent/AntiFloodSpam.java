c77380426679c72900580699aa8bdba2a1ce1784
xize <glucassen@live.nl>
2013-09-16 20:11:58 +0200

    added anti flood spam still need tweaks

--- src/tv/mineinthebox/events/chatEvent/AntiFloodSpam.java
+++ src/tv/mineinthebox/events/chatEvent/AntiFloodSpam.java
@@ -2,74 +2,39 @@ package tv.mineinthebox.events.chatEvent;
 
 import java.util.HashMap;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-import tv.mineinthebox.xEssentials;
 import tv.mineinthebox.xEssentialsMemory;
+import tv.mineinthebox.commands.cmdban;
+import tv.mineinthebox.commands.cmdban.banType;
 
 @SuppressWarnings("deprecation")
 public class AntiFloodSpam implements Listener {
 	
-	public static HashMap<String, level> messages = new HashMap<String, level>();
-	
-	public static enum level {
-		normal,
-		medium,
-		flood
-	};
+	public static HashMap<String, Long> chatTime = new HashMap<String, Long>();
 	
 	@EventHandler
-	public void check_antiflood(final PlayerChatEvent e) {
-		if(e.getMessage() != null) {
-			if(messages.containsKey(e.getPlayer().getName())) {
-				if(messages.get(e.getPlayer().getName()) == level.normal) {
-					messages.put(e.getPlayer().getName(), level.medium);
-					Bukkit.getScheduler().scheduleAsyncDelayedTask(xEssentials.getPlugin(), new Runnable() {
-						@Override
-						public void run() {
-							if(messages.containsKey(e.getPlayer().getName())) {
-								if(messages.get(e.getPlayer().getName()) == level.medium) {
-									messages.remove(e.getPlayer().getName());
-								}
-							}
-						}
-						
-					}, 3L);
-				} else if(messages.get(e.getPlayer().getName()) == level.medium) {
-					messages.put(e.getPlayer().getName(), level.flood);
-					Bukkit.getScheduler().scheduleAsyncDelayedTask(xEssentials.getPlugin(), new Runnable() {
-						@Override
-						public void run() {
-							if(messages.containsKey(e.getPlayer().getName())) {
-								if(messages.get(e.getPlayer().getName()) == level.flood) {
-									messages.remove(e.getPlayer().getName());
-								}
-							}
-						}
-						
-					}, 3L);
-				} else if(messages.get(e.getPlayer().getName()) == level.flood) {
-					e.getPlayer().getServer().dispatchCommand(Bukkit.getConsoleSender(), "mute " + e.getPlayer().getName() + " 1D");
-					e.getPlayer().kickPlayer(xEssentialsMemory.antiFloodSpamMessage);
-				}
+	public void playerChatTime(PlayerChatEvent e) {
+		if(chatTime.containsKey(e.getPlayer().getName())) {
+			if(System.currentTimeMillis() < chatTime.get(e.getPlayer().getName())) {
+				cmdban.setBanned(xEssentialsMemory.antiFloodSpamMessage, e.getPlayer(), banType.floodspam);
 			} else {
-				messages.put(e.getPlayer().getName(), level.normal);
-				Bukkit.getScheduler().scheduleAsyncDelayedTask(xEssentials.getPlugin(), new Runnable() {
-
-					@Override
-					public void run() {
-						if(messages.containsKey(e.getPlayer().getName())) {
-							if(messages.get(e.getPlayer().getName()) == level.normal) {
-								messages.remove(e.getPlayer().getName());
-							}
-						}
-					}
-					
-				}, 3L);
+				chatTime.remove(e.getPlayer().getName());
 			}
+		} else {
+			chatTime.put(e.getPlayer().getName(), System.currentTimeMillis());
+		}
+	}
+	
+	@EventHandler
+	public void pQuit(PlayerQuitEvent e) {
+		if(chatTime.containsKey(e.getPlayer().getName())) {
+			chatTime.remove(e.getPlayer().getName());
+		} else {
+			return;
 		}
 	}
 
