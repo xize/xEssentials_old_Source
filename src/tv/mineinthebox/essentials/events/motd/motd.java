abcbc06120b92ca867417da62572db0260570462
xize <glucassen@live.nl>
2013-11-13 20:13:26 +0100

    made random motd for the server list work

--- src/tv/mineinthebox/essentials/events/motd/motd.java
+++ src/tv/mineinthebox/essentials/events/motd/motd.java
@@ -1,15 +1,33 @@
 package tv.mineinthebox.essentials.events.motd;
 
+import java.util.ListIterator;
+
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
+import tv.mineinthebox.essentials.xEssentialsMemory;
+
 public class motd implements Listener {
+	
+	ListIterator<String> it = xEssentialsMemory.motdMessages.listIterator(); 
 	
 	@EventHandler
 	public void setMothd(ServerListPingEvent e) {
-		//String s = fileManager.getStringValue("motd.yml", "message", fileManager.getDir());
-		e.setMotd(ChatColor.translateAlternateColorCodes('&', "&ewe need help debugging ManCo supplycrates!"));
+		if(xEssentialsMemory.motdEnabled) {
+			e.setMotd(ChatColor.translateAlternateColorCodes('&', xEssentialsMemory.motdMessage));	
+		} else if(xEssentialsMemory.motdRandom) {
+			if(it.hasNext()) {
+				e.setMotd(ChatColor.translateAlternateColorCodes('&', it.next()));
+			} else {
+				while(it.hasPrevious()) {
+					it.previous();
+				}
+				if(it.hasNext()) {
+					e.setMotd(ChatColor.translateAlternateColorCodes('&', it.next()));
+				}
+			}
+		}
 	}
 }
