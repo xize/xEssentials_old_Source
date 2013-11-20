fb460beebd66b5a507d1f7e999a1fb1b9907fec4
xize <glucassen@live.nl>
2013-11-20 15:52:31 +0100

    updated ban event for our new lower case system

--- src/tv/mineinthebox/essentials/events/banEvents/bancheck.java
+++ src/tv/mineinthebox/essentials/events/banEvents/bancheck.java
@@ -15,16 +15,16 @@ public class bancheck implements Listener {
 	@EventHandler
 	public void checkIfBanned(PlayerJoinEvent e) {
 		if(ban.isBanned(e.getPlayer())) {
-			e.getPlayer().kickPlayer(fileManager.getStringValue(e.getPlayer().getName() + ".yml", "Reason", fileManager.getDir() + File.separator + "bans"));
+			e.getPlayer().kickPlayer(fileManager.getStringValue(e.getPlayer().getName().toLowerCase() + ".yml", "Reason", fileManager.getDir() + File.separator + "bans"));
 			e.setJoinMessage("");	
 		} else if(ban.isTempBanned(e.getPlayer())) {
-			if(timeunits.isOverTime(fileManager.getLongValue(e.getPlayer().getName() + ".yml", "time", fileManager.getDir() + File.separator + "bans"))) {
-				fileManager.writeFile(e.getPlayer().getName() + ".yml", "Tempbanned", false, fileManager.getDir() + File.separator + "bans");
-				fileManager.writeFile(e.getPlayer().getName() + ".yml", "time", 0, fileManager.getDir() + File.separator + "bans");
+			if(timeunits.isOverTime(fileManager.getLongValue(e.getPlayer().getName().toLowerCase() + ".yml", "time", fileManager.getDir() + File.separator + "bans"))) {
+				fileManager.writeFile(e.getPlayer().getName().toLowerCase() + ".yml", "Tempbanned", false, fileManager.getDir() + File.separator + "bans");
+				fileManager.writeFile(e.getPlayer().getName().toLowerCase() + ".yml", "time", 0, fileManager.getDir() + File.separator + "bans");
 				return;
 			} else {
 				e.setJoinMessage("");
-				e.getPlayer().kickPlayer("you where banned tempory by staff member " + fileManager.getStringValue(e.getPlayer().getName() + ".yml", "BannedBy", fileManager.getDir() + File.separator + "bans") + "\n till " + timeunits.getElapsedTime(fileManager.getLongValue(e.getPlayer().getName() + ".yml", "time", fileManager.getDir() + File.separator + "bans")));
+				e.getPlayer().kickPlayer("you where banned tempory by staff member " + fileManager.getStringValue(e.getPlayer().getName().toLowerCase() + ".yml", "BannedBy", fileManager.getDir() + File.separator + "bans") + "\n till " + timeunits.getElapsedTime(fileManager.getLongValue(e.getPlayer().getName() + ".yml", "time", fileManager.getDir() + File.separator + "bans")));
 			}
 		}
 	}
