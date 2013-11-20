6744352199236e95272f1e229f84fb123f877b55
xize <glucassen@live.nl>
2013-11-20 16:12:46 +0100

    allow task to read lower case files

--- src/tv/mineinthebox/essentials/events/playerEvents/taskEvent.java
+++ src/tv/mineinthebox/essentials/events/playerEvents/taskEvent.java
@@ -12,8 +12,8 @@ import tv.mineinthebox.essentials.fileManager;
 public class taskEvent implements Listener {
 	@EventHandler
 	public void HandleOnCommandTask(PlayerJoinEvent e) {
-		if(fileManager.file_exists(e.getPlayer().getName() + ".yml", fileManager.getDir() + File.separator + "tasks")) {
-			e.getPlayer().getServer().dispatchCommand(Bukkit.getConsoleSender(), fileManager.getStringValue(e.getPlayer().getName() + ".yml", "command", fileManager.getDir() + File.separator + "tasks"));
+		if(fileManager.file_exists(e.getPlayer().getName().toLowerCase() + ".yml", fileManager.getDir() + File.separator + "tasks")) {
+			e.getPlayer().getServer().dispatchCommand(Bukkit.getConsoleSender(), fileManager.getStringValue(e.getPlayer().getName().toLowerCase() + ".yml", "command", fileManager.getDir() + File.separator + "tasks"));
 		}
 	}
 }
