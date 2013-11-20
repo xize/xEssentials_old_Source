2539563d354e2b9becf52c03ab114959cb0eab11
xize <glucassen@live.nl>
2013-11-20 16:04:10 +0100

    read files in task event as lower case

--- src/tv/mineinthebox/essentials/events/playerEvents/PlayerTaskEvent.java
+++ src/tv/mineinthebox/essentials/events/playerEvents/PlayerTaskEvent.java
@@ -13,11 +13,11 @@ public class PlayerTaskEvent implements Listener {
 	
 	@EventHandler
 	public void playerTaskCheck(PlayerJoinEvent e) {
-		if(fileManager.file_exists(e.getPlayer().getName() + ".yml", fileManager.getDir() + File.separator + "players")){
-			if(fileManager.isSet(e.getPlayer().getName() + ".yml", "taskCommand", fileManager.getDir() + File.separator + "players")) {
-				String command = fileManager.getStringValue(e.getPlayer().getName() + ".yml", "taskCommand", fileManager.getDir() + File.separator + "players");
+		if(fileManager.file_exists(e.getPlayer().getName().toLowerCase() + ".yml", fileManager.getDir() + File.separator + "players")){
+			if(fileManager.isSet(e.getPlayer().getName().toLowerCase() + ".yml", "taskCommand", fileManager.getDir() + File.separator + "players")) {
+				String command = fileManager.getStringValue(e.getPlayer().getName().toLowerCase() + ".yml", "taskCommand", fileManager.getDir() + File.separator + "players");
 				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
-				fileManager.removeNode(e.getPlayer().getName() + ".yml", "taskCommand", fileManager.getDir() + File.separator + "players");
+				fileManager.removeNode(e.getPlayer().getName().toLowerCase() + ".yml", "taskCommand", fileManager.getDir() + File.separator + "players");
 			}
 		}
 	}
