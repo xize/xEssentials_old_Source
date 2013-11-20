ad53694da936dbf67ec3d974f6db9814655f048c
xize <glucassen@live.nl>
2013-11-20 16:12:30 +0100

    saveinventory properly

--- src/tv/mineinthebox/essentials/events/playerEvents/saveInventory.java
+++ src/tv/mineinthebox/essentials/events/playerEvents/saveInventory.java
@@ -13,7 +13,7 @@ public class saveInventory implements Listener {
 	@EventHandler
 	public void saveInv(PlayerQuitEvent e) {
 		if(fileManager.getBooleanValue("player.yml", "save-playerInventory", fileManager.getDir())) {
-			fileManager.writeFile(e.getPlayer().getName() + ".yml", "inv", e.getPlayer().getInventory().getContents(), fileManager.getDir() + File.separator + "inventory");
+			fileManager.writeFile(e.getPlayer().getName().toLowerCase() + ".yml", "inv", e.getPlayer().getInventory().getContents(), fileManager.getDir() + File.separator + "inventory");
 		}
 	}
 
